caac0aa52f4b153677a02a9b7c6101d1adb3cbb1
Debapriya Das <debapriyadas73@gmail.com>
2023-08-08 12:00:09 +0530

    quick_sort

--- sortings/merge_sort.py
+++ sortings/merge_sort.py
@@ -1,29 +1,25 @@
 
 
-def merge_sort(A,p,r):
-    # print("ms->",A,p,r)
-    if p<r:
+def merge_sort(A, p, r):
+    print("mergr_sort->", A, p, r)
+    if p < r:
         q = (p+r)//2
-        merge_sort(A,p,q)
-       
-        merge_sort(A,q+1,r)
-        merge(A,p,q,r)
 
-def merge(A,p,q,r):
-    
-    print("merging->",A[p:q+1],A[q+1:r+1])
+        merge_sort(A, p, q)
+        merge_sort(A, q+1, r)
 
-    n1 = q-p+1
-    n2 = r-q
+        merge(A, p, q, r)
 
-    L=[]
-    R=[]
-    
-    # copy the divided values into left and right lists
-    # for i in range(p,n1):
-    #     L.append(A[p+i])
-    # for i in range(q,n2):
-    #     R.append(A[q+1+i])
+
+def merge(A, p, q, r):
+
+    print("merging->", A[p:q+1], A[q+1:r+1])
+
+    # n1 = q-p+1
+    # n2 = r-q
+
+    L = []
+    R = []
 
     L = A[p:q+1]
     R = A[q+1:r+1]
@@ -32,35 +28,35 @@ def merge(A,p,q,r):
 
     L.append(inf)
     R.append(inf)
-    
-    print("comparing array->",L,R)
 
-    i=0
-    j=0
+    # print("comparing array->", L, R)
 
-    for k in range(p,r+1):
+    i = 0
+    j = 0
+
+    for k in range(p, r+1):
 
         # print("k =",k,"r =",r)
         # print("sorting->",L,R,p,r,i,j)
-        print("comparing->",L[i],R[j])
+        # print("comparing->", L[i], R[j])
 
-        if L[i]<=R[j]:
+        if L[i] <= R[j]:
             A[k] = L[i]
-            i+=1
+            i += 1
         else:
             A[k] = R[j]
-            j+=1
+            j += 1
         # print(i,j)
-    print("merged array =",A[p:r+1])
+    # print("merged array =", A[p:r+1])
 # calling the function
 
-A = [2,60,6,98,23,997,4,9,3,1]
+
+A = [2, 60, 6, 98, 23, 997, 4, 9, 3, 1]
 p = 0
 r = len(A)-1
 
 # print(A[p:q+1],A[q+1:r+1])
 print(A)
-merge_sort(A,p,r)
+merge_sort(A, p, r)
 
 print(A)
-    
---
+++ sortings/quick_sort.py
@@ -0,0 +1,43 @@
+import random
+
+
+def quick_sort(A, low, high):
+
+    if low < high:
+        # print("array->", A)
+        pivot = partition(A, low, high)
+        quick_sort(A, low, pivot-1)
+        quick_sort(A, pivot+1, high)
+
+
+def partition(A, low, high):
+
+    # print("partition called")
+    # print(A[low], " to ", A[high])
+
+    pivot_element = A[low]
+
+    i = low
+
+    for j in range(low+1, high+1):
+        # print(A[i], A[j])
+        # increment i if any value is less than the pivot element then swap it with current element
+        if A[j] <= pivot_element:
+            # print("less element found", A[j], pivot_element)
+            i += 1
+            if i != j:
+                # print("swapping", i, j, A)
+                A[i], A[j] = A[j], A[i]
+                # print("after swapping", A)
+
+    A[i], A[low] = A[low], A[i]
+    return i
+
+
+arr = [9, 12, 232, 123, 12, 454, 42, 3, 444]
+
+print("before sorting->", arr)
+
+quick_sort(arr, 0, len(arr)-1)
+
+print("after sorting->", arr)
